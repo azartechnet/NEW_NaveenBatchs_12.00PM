27aca8321699742321fc67389746b8448b9586f9
azartechnet <azartechnet@gmail.com>
2026-01-21 12:59:37 +0530

    updated

--- reactjs/project1/src/index.js
+++ reactjs/project1/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom/client";
+//Basic Rendering of a React Component
+/*const App=()=>{
+  return(
+    <h1>Hello, World!</h1>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById("root"))
+r1.render(<App/>)*/
+//Dynamic Rendering 
+const Sample=()=>{
+  const name="azar";
+  const age=30;
+  return(
+    <div>
+      <h1>Hello, My name is {name}.</h1>
+      <h2>I am {age} years old.</h2>
+      <p>Today Date is::{new Date().toLocaleDateString()}</p>
+    </div>
+  )
+}
+const r1=ReactDOM.createRoot(document.getElementById("root"))
+r1.render(<Sample/>)
