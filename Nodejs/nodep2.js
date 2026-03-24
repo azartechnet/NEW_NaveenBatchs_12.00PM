20eecd4cce3f78e3c545e68423e81d8af8fd228f
azartechnet <azartechnet@gmail.com>
2026-03-24 13:58:56 +0530

    updated

---
+++ Nodejs/nodep2.js
@@ -0,0 +1,5 @@
+console.log("welcome to the application")
+console.warn("This is warning")
+console.error("This is error message")
+const name="azar";
+console.log(`My name is::${name}`)
---
+++ Nodejs/nodep7.js
@@ -0,0 +1,27 @@
+//Synchrous read
+
+var f1=require('fs')
+try
+{
+    var data=f1.readFileSync('f1.txt')
+    console.log(data.toString())
+}
+catch(err)
+{
+    console.log(err)
+}
+//Asynchrous read
+
+setTimeout(function(){
+    var f1=require('fs')
+    f1.readFile('f1.txt',function(err,data){
+        if(err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            console.log(data.toString())
+        }
+    })
+},2000)
